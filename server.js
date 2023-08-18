e2e38fa362b95e9d2a039fbb8dfc2ac589caeb4c
vishnuathreya28 <vishnu.athreya77@gmail.com>
2023-08-18 16:40:24 +0530

    all changes have been made, a function to add numbers and subtract numbers has been defined and it passes all the defined test cases in the collection.json file

--- server.js
+++ server.js
@@ -10,16 +10,32 @@ app.use(express.json());
 const baseRouter = express.Router();
 
 baseRouter.get('/greeting', (req, res) => {
+    // console.log("yes API is working");
     return res.send('');
 });
 
 baseRouter.post('/add', (req, res) => {
-    res.json({ "": null });
+    const { first, second } = req.body; // Extract first and second from the request body
+
+    // Check if both first and second are valid numbers
+    if (typeof first !== 'number' || typeof second !== 'number') {
+        return res.status(400).json({ error: 'Invalid input. Both numbers should be valid.' });
+    }
+
+    const sum = first + second; // Calculate the sum of first and second
+
+    res.json({ result: sum }); // Send the sum as a JSON response
 });
 
-
 baseRouter.post('/subtract', (req, res) => {
-    res.json({ "": null });
+    const { first, second } = req.body;
+
+    if(typeof first!== 'number' || typeof second!== 'number'){
+        return res.status(400).json({error: 'Invalid input. Both numbers need to be valid.'});
+    }
+
+    const diff = first - second;
+    res.json({ result: diff });
 });
 
 app.use(baseUrl, baseRouter);
